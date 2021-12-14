a7954c2de525e6dedf4e3316c55a666cb2351a14
Chaloemwatthana Patchimma <c.patchimma@at-visions.com>
2021-12-14 12:20:02 +0100

    FINISHED Values and Variables

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,28 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Matilda";
+let first = 'Jonas';
+// let firstNamePerson;
+// let first_name_person;
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = 'JM'
+let $function = 27;
+
+let person = 'jonas'
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+
+//False
+let job1 = 'programmer';
+let job2 = 'teacher';
